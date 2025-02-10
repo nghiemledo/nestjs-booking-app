54b3d28fa102e3f3f9fc824b6ce3b813c2159562
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-02-10 17:56:15 +0700

    Feat: add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { VenuesModule } from './venues/venues.module';
+import { AvailabilityModule } from './availability/availability.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/coworking_db'),
+    UsersModule,
+    BookingsModule,
+    VenuesModule,
+    AvailabilityModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
