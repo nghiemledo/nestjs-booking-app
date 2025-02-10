52dbaef567e86c2f7508a8669d2d5514e423de31
*gWRU5u1c7q <nghiemledo@gmail.com>
2025-02-10 17:59:45 +0700

    Fix: import for venue module

--- src/venues/venues.module.ts
+++ src/venues/venues.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Venue, VenueSchema } from './venues.schema';
+import { VenuesController } from './venues.controller';
+import { VenuesService } from './venues.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Venue.name, schema: VenueSchema }]),
+  ],
+  controllers: [VenuesController],
+  providers: [VenuesService],
+})
 export class VenuesModule {}
